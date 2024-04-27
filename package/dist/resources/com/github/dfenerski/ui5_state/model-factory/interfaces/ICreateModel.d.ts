3f4cafa54be0c3f76ef7dbd179e77fe4ad4f6c92
Dimitar Fenerski <dfenerski@gmail.com>
2024-04-27 10:39:29 +0300

    chore: publish v0.0.2

--- package/dist/resources/com/github/dfenerski/ui5_state/model-factory/interfaces/ICreateModel.d.ts
+++ package/dist/resources/com/github/dfenerski/ui5_state/model-factory/interfaces/ICreateModel.d.ts
@@ -2,7 +2,7 @@ declare module "com/github/dfenerski/ui5_state/model-factory/interfaces/ICreateM
     import BindingMode from 'sap/ui/model/BindingMode';
     import JSONModel from 'sap/ui/model/json/JSONModel';
     interface ICreateModel<T extends {}> {
-        readonly data: T;
+        readonly data: T | string;
         readonly bindingMode?: BindingMode;
         readonly sizeLimit?: number;
         readonly skipRegistration?: boolean;
